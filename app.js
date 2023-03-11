0f276366bb8f02e087b8cd42d9b11fd2bd328e5d
star <chlvkfksquf@gmail.com>
2023-03-11 23:36:58 +0900

    Modify app

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import cors from "cors";
 
 import express from "express";
+import bodyParser from "body-parser";
+
+import userRouter from "./routes/userRouter.js";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/users", userRouter);
 
 export default app;
