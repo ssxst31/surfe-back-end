1b0cc45e260c15057d9adf52fe2ad42dcd8fdf79
star <chlvkfksquf@gmail.com>
2023-03-12 02:04:47 +0900

    Modify cors

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import cors from "cors";
-
-import express from "express";
 import bodyParser from "body-parser";
 
+import express from "express";
 import userRouter from "./routes/userRouter.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
