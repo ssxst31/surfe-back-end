6929fbea9f0f3438fe0a679ea3b3d7894d0dcd88
star <chlvkfksquf@gmail.com>
2023-03-23 20:05:58 +0900

    Add roomName

--- datasources/ws.js
+++ datasources/ws.js
@@ -26,14 +26,13 @@ class WebSocket {
   }
 
   onConnect(client) {
-    const roomName = "room1";
-    client.join(roomName);
-
     client.on("JOIN_ROOM", (data) => {
+      const roomName = data.roomName;
+      client.join(roomName);
       console.log(`${data.nickname}님이 입장하였습니다.`);
 
       getConnection((conn) => {
-        const sql1 = `SELECT * FROM chat`;
+        const sql1 = `SELECT * FROM chat WHERE roomName LIKE '${data.roomName}'`;
         conn.query(sql1, (error, rows) => {
           return this.io
             .to(roomName)
@@ -44,24 +43,72 @@ class WebSocket {
       });
     });
 
-    client.on("SEND_MESSAGE", (data) => {
-      const { content, nickname } = data;
+    client.on("JOIN_PRIVATE_ROOM", (data) => {
+      console.log(`${data.nickname}님이 입장하였습니다.`);
+      client.join(data.roomName);
 
       getConnection((conn) => {
+        const sql1 = `SELECT * FROM chat WHERE roomName LIKE '${data.roomName}'`;
+
+        conn.query(sql1, (error, rows) => {
+          console.log(rows);
+          return this.io
+            .to(data.roomName)
+            .emit("RECEIVE_PRIVATE_MESSAGE", { chatList: rows });
+        });
+
+        conn.release();
+      });
+    });
+
+    client.on("SEND_MESSAGE", (data) => {
+      const { content, nickname } = data;
+      const roomName = data.roomName;
+      console.log(roomName);
+      client.join(data.roomName);
+      getConnection((conn) => {
         conn.query(
-          "INSERT INTO chat ( content, nickname, createAt ) VALUES ?;",
-          [[[content, nickname, new Date()]]],
+          "INSERT INTO chat ( content, nickname, createAt, roomName ) VALUES ?;",
+          [[[content, nickname, new Date(), roomName]]],
           (error) => {
             if (error) {
               return console.log(error);
             }
 
-            const sql1 = `SELECT * FROM chat`;
+            const sql1 = `SELECT * FROM chat WHERE roomName LIKE '${roomName}'`;
 
             conn.query(sql1, (error, rows) => {
               return this.io
                 .to(roomName)
                 .emit("RECEIVE_MESSAGE", { chatList: rows });
+            });
+          }
+        );
+
+        conn.release();
+      });
+    });
+
+    client.on("SEND_PRIVATE_MESSAGE", (data) => {
+      const { content, nickname } = data;
+      const roomName = data.roomName;
+      client.join(roomName);
+      getConnection((conn) => {
+        conn.query(
+          "INSERT INTO chat ( content, nickname, createAt, roomName ) VALUES ?;",
+          [[[content, nickname, new Date(), roomName]]],
+          (error) => {
+            if (error) {
+              return console.log(error);
+            }
+
+            const sql1 = `SELECT * FROM chat WHERE roomName LIKE '${roomName}'`;
+            console.log(content, nickname, roomName);
+            conn.query(sql1, (error, rows) => {
+              console.log(rows);
+              return this.io
+                .to(roomName)
+                .emit("RECEIVE_PRIVATE_MESSAGE", { chatList: rows });
             });
           }
         );
