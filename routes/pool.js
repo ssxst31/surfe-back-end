7eb76c1bd0a1d485315653baf4edbf75a3a65170
star <chlvkfksquf@gmail.com>
2023-03-26 13:13:51 +0900

    Add isProduction

--- routes/pool.js
+++ routes/pool.js
@@ -2,16 +2,25 @@ import mysql from "mysql2";
 
 import dotenv from "dotenv";
 
+function isProduction() {
+  return process.env.NODE_ENV === "production";
+}
+
 dotenv.config();
 
-const dbConfig = {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  multipleStatements: true,
-};
+const dbConfig = isProduction()
+  ? {
+      uri: process.env.DATABASE_URL,
+      multipleStatements: true,
+    }
+  : {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      multipleStatements: true,
+    };
 
 const pool = mysql.createPool(dbConfig);
 
