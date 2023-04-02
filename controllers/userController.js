3b7841f143011bd0543dece72c1c4886de512cdd
star <chlvkfksquf@gmail.com>
2023-04-02 19:37:54 +0900

    Clean code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,18 @@
 import { StatusCodes } from "http-status-codes";
 
-import { verifyToken } from "../utils/authorizeUtils.js";
 import getConnection from "../routes/pool.js";
 import { getDistance } from "../utils/map.js";
 
 export const userListByMeDistance = async (req, res) => {
   getConnection((conn) => {
     conn.query(
-      `SELECT * FROM chat.users WHERE email LIKE '${req.email}';`,
+      `SELECT users.id, location.lat, location.lng, users.nickname FROM location JOIN users ON users.email = location.email WHERE location.email LIKE '${req.email}'`,
       (error, rows1) => {
         if (error) {
           return console.log(error);
         }
 
-        const sql1 = `SELECT * FROM users`;
+        const sql1 = `SELECT users.id, location.lat, location.lng, users.nickname FROM location JOIN users ON users.email = location.email`;
 
         conn.query(sql1, (error, rows) => {
           let userList = [];
@@ -21,10 +20,10 @@ export const userListByMeDistance = async (req, res) => {
           for (let i = 0; i < rows.length; i++) {
             if (
               getDistance(
-                rows1[0].lat,
-                rows1[0].lng,
-                rows[i].lat,
-                rows[i].lng
+                Number(rows1[0].lat),
+                Number(rows1[0].lng),
+                Number(rows[i].lat),
+                Number(rows[i].lng)
               ) /
                 1000 <
                 5 &&
@@ -48,9 +47,8 @@ export const userListByMeDistance = async (req, res) => {
 export const profile = async (req, res) => {
   getConnection((conn) => {
     const sql1 = `SELECT users.email, users.id, location.lat, location.lng, users.nickname FROM location JOIN users ON users.email = location.email WHERE location.email LIKE '${req.email}'`;
-    console.log(sql1);
+
     conn.query(sql1, (error, rows) => {
-      console.log(rows[0]);
       return res.status(StatusCodes.OK).send(rows[0]);
     });
 
@@ -60,7 +58,7 @@ export const profile = async (req, res) => {
 
 export const location = async (req, res) => {
   const { lat, lng, email } = req.body;
-  console.log(lat, lng, email);
+
   getConnection((conn) => {
     const sql1 = `INSERT INTO location (email, lat, lng ) VALUES ('${email}', '${lat}', '${lng}' ) ON DUPLICATE KEY UPDATE email = '${email}', lat = '${lat}', lng = '${lng}'`;
 
