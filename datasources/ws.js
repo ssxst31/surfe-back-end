5ab8397b363fabd2f6c6a4b20427120399327434
star <chlvkfksquf@gmail.com>
2023-09-26 20:12:59 +0900

    Delete chat join chat list

--- datasources/ws.js
+++ datasources/ws.js
@@ -30,37 +30,6 @@ class WebSocket {
     client.on("JOIN_ROOM", (data) => {
       const roomName = data.roomName;
       client.join(roomName);
-
-      getConnection((conn) => {
-        const pageSize = 30;
-        const sql1 = `
-        SELECT chat.id, chat.message, chat.created_at, chat.room_id, chat.sender_id, user.nickname, user.profile_image
-        FROM chat 
-        JOIN user on user.id = chat.sender_id
-        WHERE room_id LIKE '${roomName}' 
-        ORDER BY chat.id DESC 
-        LIMIT ${pageSize};
-        `;
-
-        conn.query(sql1, (error, rows) => {
-          rows = rows.map((row) => {
-            return {
-              id: row.id,
-              message: row.message,
-              createdAt: row.created_at,
-              senderId: row.sender_id,
-              nickname: row.nickname,
-              profileImage: row.profile_image,
-            };
-          });
-
-          return this.io
-            .to(roomName)
-            .emit("RECEIVE_MESSAGE", { chatList: rows.reverse() });
-        });
-
-        conn.release();
-      });
     });
 
     client.on("JOIN_PRIVATE_ROOM", (data) => {
